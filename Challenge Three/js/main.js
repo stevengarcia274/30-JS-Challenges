620d4cd52591ec3a0f4b14c5a12b325c750e0caf
stevengarcia274 <s.garcia6174@gmail.com>
2023-05-26 15:28:07 -0700

    v.1 css vars with JS

--- Challenge Three/js/main.js
+++ Challenge Three/js/main.js
@@ -2,28 +2,39 @@ const r = document.querySelector(":root");
 
 let space = document.getElementById("s-slider");
 let blur = document.getElementById("b-slider");
+let color = document.getElementById("base-picker");
+
 
 
 blur.addEventListener("change", setBlur);
 space.addEventListener("change", setSpace);
+color.addEventListener("change", setColor);
 
 
 
 function myFunc(){
     
-    console.log(blur.value);
+    color.value = "#FF8040";
+
+}
+
+
+function setColor(){
+    let colorVal = color.value;
+    console.log(colorVal);
+    r.style.setProperty("--base-pick", `${colorVal}` );
 
 }
 
 function setSpace(){
     let spaceVal = space.value;
-    console.log(spaceVal);
+    /* console.log(spaceVal); */
     let spaceCalc = spaceVal * 4;
     r.style.setProperty("--spacing-amnt", `${spaceCalc}px` );
 }
 
 function setBlur(){
     let newVal = blur.value;
-    console.log(newVal);
+    /* console.log(newVal); */
     r.style.setProperty("--img-blur", `${newVal}px` );
 }

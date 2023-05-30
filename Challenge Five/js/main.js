244aa57aba41015d66b0879625780fbf965183de
stevengarcia274 <s.garcia6174@gmail.com>
2023-05-30 11:37:35 -0700

    comments added

--- Challenge Five/js/main.js
+++ Challenge Five/js/main.js
@@ -15,23 +15,31 @@ conItems.forEach(e => e.addEventListener("click", flexGrow));
 
 
 function flexGrow(){
-    /* let target = this.getAttribute("name");
-    conItems.forEach(e => e.getAttribute("name") != target ? e.style.flex = "none" : e.style.flex = "1"); */
-    const current = this.getAttribute("id");
+    //current item that was clicked on
+    const current = this.getAttribute("id"); 
+
     console.log(current);
     console.log("before " + this.dataset.value);
+
     if (this.dataset.value === "true"){
-        /* this.dataset.value = "false" */
+        /* if value is equal to true before the 
+        item is clicked that means we must TURN OFF the current item */
         conItems.forEach(function (e){
+            /* all items need to be set back to 1 and values switched to off */
             e.style.flex = "1";
             e.dataset.value = "false";
         });
     }else{
-        this.dataset.value = "true";
+        /* else this means that the item that was clicked needs to grow */
+        this.dataset.value = "true"; 
+        //set its grow value to true
         conItems.forEach(function (e){
+            // go through each item 
             if(e.getAttribute("id") === current ){
+                /* if it IS the currently clicked item set its flex grow to 3*/
                 e.style.flex = "3";
             }else{
+                /* if the item during iteration does not match our current clicked item TURN its value to false (off) and set its flex to 1 */
                 e.style.flex ="1";
                 e.dataset.value = "false";
             }

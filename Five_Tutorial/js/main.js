3d6e763812eabb8af39b212dc78e5ae7c60ddfdc
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-02 13:07:50 -0700

    finsihed code effective solution

--- Five_Tutorial/js/main.js
+++ Five_Tutorial/js/main.js
@@ -2,8 +2,26 @@ const container = document.querySelector(".container");
 
 const conItems = container.querySelectorAll(".item");
 
-conItems.forEach(item => item.addEventListener("click", toggleOn));
+
 
 function toggleOn(){
     conItems.forEach(item => item.getAttribute("id") !== this.getAttribute(("id")) ? item.classList.remove("grow") : item.classList.toggle("grow"));
 }
+
+function toggleActive(e){
+    console.log(e);
+    console.log(e.propertyName);
+    
+    if(e.propertyName.includes("flex")){
+        this.classList.toggle("grow-active");
+    }
+    
+}
+
+/* id rather be pushing flowers then to be in the pen sharing showers tony 
+ton */
+
+//mouseover looks kind of cool too
+conItems.forEach(item => item.addEventListener("click", toggleOn));
+
+conItems.forEach(item => item.addEventListener("transitionend", toggleActive));

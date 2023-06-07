5bd9be675cf848984d382b757bd56057a2098324
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-07 12:03:09 -0700

    async tutorial finsihed.

--- async_tutorial.js
+++ async_tutorial.js
@@ -1,23 +1,54 @@
- const stocks = {
-    fruits : ["strawberry", "grapes", "banana", "apple"],
+const stocks = {
+    fruits : ["strawberry", "grape", "banana", "apple"],
     liquid : ["water", "ice"],
     holder : ["cone", "cup", "stick"],
     toppings : ["chocolate", "peanuts"],
- };
+};
 
- let isShopOpen = true;
+let isShopOpen = true;
 
-
- let order = (time, work) => {
-
-    return new Promise( (resolve, reject) => {
-
+function time(ms){
+    return new Promise( (resolve, reject ) => {
         if(isShopOpen){
-            resolve( work() );
+            setTimeout(resolve, ms);
+            
         }else{
-            reject(console.log("our shop is closed"));
+            reject(console.log("shop is closed"));
         }
+    })
+}
 
+async function kitchen(){
+    try{
+        await time(2000);
+        console.log(`${stocks.fruits[0]} chosen`);
 
-    });
- };
+        console.log("start production");
+
+        await time(2000);
+        console.log("fruit is chopped");
+
+        await time(1000);
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`);
+
+        await time(1000);
+        console.log("machine started");
+
+        await time(2000);
+        console.log(`${stocks.holder[1]} selected as container`);
+
+        await time(2000);
+        console.log("ice cream served");
+
+        
+    }
+    catch(error){
+        console.log("customer left");
+
+    }
+    finally{
+        console.log("day is done");
+    }
+}
+
+kitchen();

4512e81e004249b13a3264917910ca7df3851ff7
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-08 14:01:12 -0700

    finished ajax/json tutorial

--- JSON_AJAX Tutorial/js/main.js
+++ JSON_AJAX Tutorial/js/main.js
@@ -1,13 +1,47 @@
-// https://learnwebcode.github.io/json-example/animals-1.json
 
-const ourRequest = new XMLHttpRequest();
+const infoButton = document.getElementById("info");
+const infoContainer = document.getElementById("add-info");
 
-ourRequest.open("get", "https://learnwebcode.github.io/json-example/animals-1.json");
+let count = 1;
 
-ourRequest.onload = function(){
+let conNodeList;
 
-    let info = JSON.parse(ourRequest.responseText);
-    console.log(info[0]);
-};
+function loadThree(){
+    const petRequest = new XMLHttpRequest;
 
-ourRequest.send();
+    petRequest.open("get", `https://learnwebcode.github.io/json-example/animals-${count}.json`);
+
+    petRequest.onload = function(){
+        /* if(this.status == 404){
+            document.getElementById("info").style.display = "none";
+        } */
+        if(count == 3){
+            document.getElementById("info").style.display = "none";
+        }
+
+        let info = JSON.parse(petRequest.responseText);
+
+        for (let x in info){
+            let current = info[x];
+            let paraNode = document.createElement("p");
+            let textNode = document.createTextNode(`${current.name} is a ${current.species} that likes to eat ${current.foods.likes[0]} and dislikes ${current.foods.dislikes[0]}.`);
+
+            paraNode.classList.add("info-item");
+
+            paraNode.appendChild(textNode);
+
+            infoContainer.appendChild(paraNode);
+
+        }
+
+        conNodeList = infoContainer.querySelectorAll("p");
+
+        count++;   
+    }
+
+    petRequest.send();
+}
+
+
+infoButton.addEventListener("click", loadThree);
+

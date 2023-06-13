07de6a8075ab2f6eb5f8fe82d1c8d7fcdcd616ca
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-12 17:05:31 -0700

    Functioning typeAhead. Check work with tutorial.

--- Challenge Six/js/main.js
+++ Challenge Six/js/main.js
@@ -1,23 +1,20 @@
+//set up commonly referenced elements
 const searchBar = document.getElementById("search");
 
-const clickBtn = document.getElementById("click");
+const searchResults = document.getElementById("results-container");
 
-const hello = "hello";
-
+//keyup event listener to detect search
 searchBar.addEventListener("keyup", () => {
-    getData(searchBar.value);
+    getData(searchBar.value)
 });
 
+//eventlistener to avoid results just hanging there
 
-function printHi(hello){
-    console.log(hello);
-}
+searchBar.addEventListener("focusout", clearDemo);
 
-function callData(search){
-    getData(search);
-}
 
 function getData(search) { 
+    //XMLrequest
     const dataPull = new XMLHttpRequest;
      
 
@@ -28,15 +25,35 @@ function getData(search) {
 
         let x = jsonData.filter(e => e.city.match(searchExp) || e.state.match(searchExp));
 
+        let sortedX = x.sort( (a,b) => b.population - a.population);
+
+        /* attempt at error handling, when search is invalid/yields nothing */
         try{
-            x[0].city;
+            //building html elements for results
+            let i = 0 
+            let txt = ``;
+            if(x.length === 0){//no results
+                searchResults.innerHTML = `<p class="result">No Results.</p>`;
+            }else if(search === ""){//searchbar is active but no value
+                searchResults.innerHTML = ``;
+            }else{
+                /* I only want to display the top ten results based on population */
+                while(i < (x.length > 10 ? 10 : x.length)){
+                    //if there's less then ten returned only show those
+                    txt += `<p class="result">${sortedX[i].city}, ${sortedX[i].state}
+                    <span class="pop">${sortedX[i].population}</span></p>`;
+                    i++;
+                }
+
+                //add results to div
+                searchResults.innerHTML = txt;
+            }
+                
         }catch(error){
-            console.log("not valid");
+            console.log("No Results.");
         }
 
-        document.getElementById("demo").innerHTML = x[0].city;
-        
-
+        /* console.log(x); */    
     }
 
     dataPull.open("get", "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json");
@@ -45,62 +62,6 @@ function getData(search) {
 
 }
 
-/* function findMatches(search, data){
-    const searchExp = new RegExp(search, "gi");
-
-    let x = data.filter(e => e.city.match(searchExp) || e.state.match(searchExp));
-
-    document.getElementById("demo").innerHTML = x[0];
-    console.log(x[0]);
-    
-} */
-
-
-/* getData("iowa", findMatches);
- */
-
-
-
-
-
-
-
-
-const data = [
-    {
-        "city": "New York", 
-        "growth_from_2000_to_2013": "4.8%", 
-        "latitude": 40.7127837, 
-        "longitude": -74.0059413, 
-        "population": "8405837", 
-        "rank": "1", 
-        "state": "New York"
-    }, 
-    {
-        "city": "Los Angeles", 
-        "growth_from_2000_to_2013": "4.8%", 
-        "latitude": 34.0522342, 
-        "longitude": -118.2436849, 
-        "population": "3884307", 
-        "rank": "2", 
-        "state": "California"
-    }, 
-    {
-        "city": "Chicago", 
-        "growth_from_2000_to_2013": "-6.1%", 
-        "latitude": 41.8781136, 
-        "longitude": -87.6297982, 
-        "population": "2718782", 
-        "rank": "3", 
-        "state": "Illinois"
-    }, 
-    {
-        "city": "Houston", 
-        "growth_from_2000_to_2013": "11.0%", 
-        "latitude": 29.7604267, 
-        "longitude": -95.3698028, 
-        "population": "2195914", 
-        "rank": "4", 
-        "state": "Texas"
-    }
-];
+function clearDemo(){
+    searchResults.innerHTML = "";
+}

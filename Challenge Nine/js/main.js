6085c83fac9fb8571aa0de618889c7e67bd3de53
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-21 17:12:06 -0700

    testing stuff out

--- Challenge Nine/js/main.js
+++ Challenge Nine/js/main.js
@@ -1,7 +1,7 @@
 const boxes = document.querySelectorAll(".choice");
 
-let lastClicked;
-let prvsClicked;
+let lastClicked = "";
+let prvsClicked = "";
 console.log(boxes);
 
 
@@ -16,15 +16,64 @@ console.log(c2.checked); */
 
 boxes.forEach(box => {
     box.addEventListener("click", (e) => {
-        
+        let curBox = document.getElementById(box.id);
+        if(curBox.checked == false){
+            /* prvsClicked = lastClicked;
+            lastClicked = ""; */
+
+            lastClicked = prvsClicked;
+            prvsClicked = "";
+
+            console.log(prvsClicked + "prvs");
+            console.log(lastClicked + "last");
+            return
+        }else{
+            if(lastClicked == ""){
+                prvsClicked = prvsClicked;
+            }else{
+                prvsClicked = lastClicked;
+
+            }
+            lastClicked = box.id;
+            console.log(prvsClicked + "prvs");
+            console.log(lastClicked + " last");
+
+        }
     });
+
+    box.addEventListener("keydown", (e) => {
+        if(e.key == "Shift"){
+            console.log(e);
+            let indexStart = prvsClicked.split("-");
+            console.log(indexStart);
+            console.log(prvsClicked + "prev");
+            console.log(lastClicked + "last");
+            window.addEventListener("mousemove", () => {
+                document.querySelector(".checkbox-container").style.background = "red";
+            })
+
+        }
+
+
+    })
     
 });
 
-window.addEventListener("keyup", (e) => {
-    if(e.key == 'Shift'){
-        console.log(e.key);
-    }else{
+
+/* let startAt = lastClicked;
+
+window.addEventListener("mousemove", (e) => {
+    
+    if(e.shiftKey == true){
+        
+        if(prvsClicked != "" && lastClicked != ""){
+            console.log(prvsClicked + "hi");
+            console.log(lastClicked);
+
+        }
+    }
+    else{
         return
     }
 });
+ */

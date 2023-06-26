e048f71c626819ead146d72b52ce60d315a7bd1c
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-25 18:29:03 -0700

    Play button done. Need progress bar.

--- Challenge Ten/js/main.js
+++ Challenge Ten/js/main.js
@@ -1,5 +1,42 @@
+const vidPlayer = document.querySelector(".video-player");
 const video = document.getElementById("video-one");
+const vidControls = document.querySelector(".video-controls");
+const progressBar = document.getElementById("progress-bar")
+const videoButtons = document.querySelector(".video-buttons");
+const play = document.getElementById("display");
 
-video.addEventListener("mouseover", () => {
-    video.style.border = "2px solid red";
+
+
+let isPlaying = false;
+
+function playVideo(){
+    isPlaying = !isPlaying;
+    if(isPlaying == true){
+        video.play();
+        document.getElementById("display").innerHTML = '<i class="fa-solid fa-pause"></i>';
+    }else{
+        video.pause();
+        document.getElementById("display").innerHTML = '<i class="fa-solid fa-play"></i>';
+
+    }
+
+
+
+}
+
+
+play.addEventListener("click", playVideo);
+
+vidPlayer.addEventListener("mouseenter", () =>{
+    progressBar.style.margin = "8px 0px";
+    videoButtons.style.display = "block";
+    
 });
+
+vidPlayer.addEventListener("mouseleave", () =>{
+    progressBar.style.margin = "0px";
+    videoButtons.style.display = "none";
+
+    
+});
+

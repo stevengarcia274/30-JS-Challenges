3df89f80977872c1419faf4d386970b18cac3dbe
stevengarcia274 <s.garcia6174@gmail.com>
2023-06-27 16:57:36 -0700

    Went thriugh tutorial redo.

--- Challenge Ten/js/main.js
+++ Challenge Ten/js/main.js
@@ -3,14 +3,13 @@ const video = document.getElementById("video-one");
 const progressBar = document.getElementById("progress-bar")
 const videoButtons = document.querySelector(".video-buttons");
 const play = document.getElementById("display");
-const volSlider = document.getElementById("vol-slider");
+const volume = document.getElementById("vol-slider");
 const speed = document.getElementById("speed-slider");
 const forward = document.getElementById("forward");
 const backward = document.getElementById("backward");
 
 
 let isPlaying = false;
-let atEnd = false;
 
 let interval;
 
@@ -28,54 +27,68 @@ function playVideo(){
     }
 }
 
+function updateProgressBar(value){
+    progressBar.style.width = `${value}%`;
+}
+
 function updateTime(){
-    /* console.log(video.currentTime); */
-    /* console.log("here"); */
+    //calculate percentage of time, to update progress bar
     let num = (video.currentTime / video.duration) * 100;
-    /* console.log(num.toFixed(2)); */
-    progressBar.style.width = `${num.toFixed(2)}%`;
+    //update progress bar
+    /* progressBar.style.width = `${num.toFixed(2)}%`; */
+    updateProgressBar(num.toFixed(2));
+    //stop set interval if we go over the time
     if(video.currentTime >= video.duration){
         clearInterval(interval);
         play.innerHTML = '<i class="fa-solid fa-play"></i>'; 
         video.load();
 
-        /* console.log("end"); */
     }
 }
 
+//play button event listener
 play.addEventListener("click", playVideo);
 
+/* hit play, start the set interval. Only if video is 
+moving forward*/
 play.addEventListener("click", () => {
     if(isPlaying){
         interval = setInterval(updateTime, 100);
     }else{
+        //else we know video was just paused, end the interval
         clearInterval(interval);
     }
     
 });
 
-
+/* bring up the rest of the controls and adjust prgress bar height when hovering over video*/
 vidPlayer.addEventListener("mouseenter", () =>{
     progressBar.style.height = "12px";
-    videoButtons.style.display = "block";
+    videoButtons.style.display = "flex";
 
 });
 
+//hide controls again
 vidPlayer.addEventListener("mouseleave", () =>{
     progressBar.style.height = "8px";
     videoButtons.style.display = "none";
     
 });
 
-volSlider.addEventListener("mousemove", () => {
-    video.volume = volSlider.value / 100;
+
+//set volume to eqaul slider value
+volume.addEventListener("mousemove", () => {
+    //must be number between 0 and 1
+    video.volume = volume.value / 100;
 });
 
-volSlider.addEventListener("click", () => {
-    video.volume = volSlider.value / 100;
+volume.addEventListener("click", () => {
+    video.volume = volume.value / 100;
 });
 
 speed.addEventListener("mousemove", () => {
+    //must be a number between 0.5 and 2
+    //look at the min and max of the slider in HTML
     video.playbackRate = speed.value / 100;
 });
 
@@ -84,21 +97,37 @@ speed.addEventListener("click", () => {
 });
 
 forward.addEventListener("click", () => {
+    //point in vid to skip to
     let jumpTo = video.currentTime + forwardAmnt;
     if(jumpTo >= video.duration){
-        play.innerHTML = '<i class="fa-solid fa-play"></i>';
-        video.load();
+        //what to do if we go over the time
+        video.currentTime = 0;
+        play.innerHTML = '<i class="fa-solid fa-play"></i>'; 
+        updateProgressBar(100);
+        video.pause();
 
     }else{
         video.currentTime = jumpTo;
         let num = (jumpTo / video.duration) * 100;
-        progressBar.style.width = `${num.toFixed(2)}%`;
+        /* progressBar.style.width = `${num.toFixed(2)}%`; */
+        updateProgressBar(num.toFixed(2));
     }
 });
 
 backward.addEventListener("click", () => {
+    //point in vid to skip back to
     let backTo = video.currentTime - backwardAmnt;
-    video.currentTime = backTo;
+    if(backTo <= 0){
+        /* progressBar.style.width = `0%`; */
+        updateProgressBar(0);
+        video.currentTime = 0;
+    }else{
+        video.currentTime = backTo;
+        let num = (backTo / video.duration) * 100;
+        /* progressBar.style.width = `${num.toFixed(2)}%`; */
+        updateProgressBar(num.toFixed(2));
+
+    }
 });
 
 

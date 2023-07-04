f47b8da4ef80ee115b62fb2ec537bb646e6e50d1
stevengarcia274 <s.garcia6174@gmail.com>
2023-07-04 15:16:48 -0700

    slight work

---
+++ Challenge Twelve/js/main.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body");
+
+window.addEventListener("scroll", (e) => {
+    console.log(window.scrollY);
+    console.log(window.innerHeight);
+    console.log(document.innerHeight);
+
+});

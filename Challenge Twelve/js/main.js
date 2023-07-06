4118f5ac6a87f61435dfef5265d4136abdc48046
stevengarcia274 <s.garcia6174@gmail.com>
2023-07-05 17:07:55 -0700

    Getting the function to be more precise

--- Challenge Twelve/js/main.js
+++ Challenge Twelve/js/main.js
@@ -1,8 +1,29 @@
-let body = document.querySelector("body");
+const falls = document.getElementById("falls");
+
+const fallsPosition = falls.getBoundingClientRect();
+const fallsPositionY = fallsPosition.y + window.scrollY;
+
+const leeway = 200;
+
+let bringForward = false;
+let pastStatus;
 
 window.addEventListener("scroll", (e) => {
-    console.log(window.scrollY);
-    console.log(window.innerHeight);
-    console.log(document.innerHeight);
+    /* console.log(e); */
+    /* console.log(window.scrollY); */
+    pastStatus = bringForward;
 
+    if(window.scrollY > fallsPositionY - leeway){
+        bringForward = true;
+    }else{
+        bringForward = false;
+    }
+
+    if(pastStatus != bringForward){
+        console.log("hello");
+    }
+
+    
 });
+
+console.log(fallsPositionY + (falls.style.height / 2));
